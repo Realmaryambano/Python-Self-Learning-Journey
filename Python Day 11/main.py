5402a409342469850efc2deee1fe16a46a22c8c9
Maryam Bano <maryambano.official@gmail.com>
2025-10-12 19:42:30 +0500

    Update main.py
    Add Day 11 folder with new code

--- Python Day 11/main.py
+++ Python Day 11/main.py
@@ -1,1 +1,9 @@
+import random
 print("Hello I am sick ")
+choice = random.randint(0,3)
+print(choice)
+
+# 0 = take rest
+# 1 = keep studying
+# 2 = no gain without pain
+# 3 = sickness is just illusion
